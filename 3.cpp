58e8fb2a57e8b1feeb43dac6efab44669e699379
vinayakdp <143714589+vinayakdp@users.noreply.github.com>
2023-08-31 17:47:52 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int c=45;
+
+
+int main(){
+    int a,b,c;
+    cout<<"eneter a";
+    cin>>a;
+    cout<<"enter b";
+    cin>>b;
+    c=a+b;
+    cout<<"sum is"<<c;
+    cout<<"sum is "<<::c;
+    return 0;
+
+}
